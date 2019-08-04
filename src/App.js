c2d1d3700665fbc5bde5b2f4f33190f6711467c2
Sanzeevtheengtamang <sanzeevtamang@gmail.com>
2019-08-04 23:39:24 +0545

    Normal Todo List

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 export default class App extends Component {
   state = {
@@ -17,12 +18,22 @@ export default class App extends Component {
       todos
     })
   }
+
+  addTodo = (todo) => {
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+
+  }
  
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo} />
       </div>
     )
   }
